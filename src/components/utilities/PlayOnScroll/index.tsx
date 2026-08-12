23bb6521504c5bfbd75a8506433e96fa29d9d903
sarthakkaushik927 <sarthakkaushik927@gmail.com>
2026-08-12 16:57:36 +0530

    fix(types): resolve TypeScript compiler errors in PlayOnScroll and CommunityMeetingsCardGrid
    Signed-off-by: sarthakkaushik927 <sarthakkaushik927@gmail.com>

--- src/components/utilities/PlayOnScroll/index.tsx
+++ src/components/utilities/PlayOnScroll/index.tsx
@@ -7,7 +7,7 @@ interface VideoProps {
     posterImg?: string;
   }
 
-const PlayOnScroll: React.FC = (props: VideoProps) => {
+const PlayOnScroll: React.FC<VideoProps> = (props: VideoProps) => {
     const videoRef = useRef<HTMLVideoElement>(null);
   
     useEffect(() => {
